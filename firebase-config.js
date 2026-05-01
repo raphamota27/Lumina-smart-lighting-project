70f69b16e8d6750c504e4563ad228ecd5276c42e
raphamota27 <rapha.mota27@gmail.com>
2026-05-01 05:11:50 -0300

    f

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCxWhWhDLdQ...", 
-  authDomain: "lumina-project-af644.firebaseapp.com",
-  projectId: "lumina-project-af644",
-  storageBucket: "lumina-project-af644.firebasestorage.app",
-  messagingSenderId: "390118211680",
-  appId: "1:390118211680:web:6ecda70fc0f199d1888305",
-  measurementId: "G-EVWHNLW39J"
-};
